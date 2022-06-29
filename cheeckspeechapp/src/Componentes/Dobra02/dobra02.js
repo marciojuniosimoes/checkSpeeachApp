8749aea93d4b8f93edebe05ba5d7a190a05e9197
Márcio Júnio <89035457+marciojuniosimoes@users.noreply.github.com>
2022-06-29 11:08:57 -0300

    dobras123

---
+++ cheeckspeechapp/src/Componentes/Dobra02/dobra02.js
@@ -0,0 +1,33 @@
+import "./dobra02.css";
+import Cliente1 from '../../assets/cliente1.svg'
+import Cliente2 from '../../assets/cliente2.svg'
+import Cliente3 from '../../assets/cliente3.svg'
+import Cliente4 from '../../assets/cliente4.svg'
+
+import React from "react";
+
+const dobra02 = () => {
+  return (
+    <div className="container-principal-dobra02">
+      <div className="card-text">
+        <h1>Clientes</h1>
+      </div>
+      <div className="card-clientes">
+        <div className="card-coluna1">
+        <img className="clienteNet" src={Cliente1} /> 
+        <img className="clientes" src={Cliente2} /> 
+
+        </div>
+        <div className="card-coluna2">
+        <img className="clientesTerra" src={Cliente3} /> 
+        <img className="clientesMinty" src={Cliente4} /> 
+
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default dobra02;
